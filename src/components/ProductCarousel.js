1925cc6efde8f996aee9ac23daabf64e9767da66
mfazans23 <mfaza16717@mail.ugm.ac.id>
2023-05-05 20:08:25 +0700

    fix carousel

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -23,18 +23,19 @@ const ProductCarousel = () => {
     <Message variant='danger'>{error}</Message>
   ) : (
     <Carousel pause='hover' className='bg-dark'>
-      {products.map((product) => (
-        <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} fluid />
-            <Carousel.Caption className='carousel-caption'>
-              <h2>
-                {product.name} ({formatPrice(product.price)})
-              </h2>
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
+      {products &&
+        products.map((product) => (
+          <Carousel.Item key={product._id}>
+            <Link to={`/product/${product._id}`}>
+              <Image src={product.image} alt={product.name} fluid />
+              <Carousel.Caption className='carousel-caption'>
+                <h2>
+                  {product.name} ({formatPrice(product.price)})
+                </h2>
+              </Carousel.Caption>
+            </Link>
+          </Carousel.Item>
+        ))}
     </Carousel>
   )
 }
